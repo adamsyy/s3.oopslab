a96343031f7bafbbf27a8d91ea789b41927f1da0
adamsy <adamoommenjacob@gmail.com>
2021-12-04 19:34:05 +0530

    prime

---
+++ Lab_Cycle/Cycle_3/Primeornot.java
@@ -0,0 +1,24 @@
+package Lab_Cycle.Cycle_3;
+
+import java.util.Scanner;
+
+public class Primeornot {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number");
+    int n=sc.nextInt();
+System.out.println(isPrime(n));
+    }
+
+    public static boolean isPrime(int n) {
+        if(n==1||n==0){
+            return false;
+        }
+        for(int i=2;i<=n/2;i++) {
+            if(n%i==0) {
+                return false;
+            }
+        }
+        return true;
+}
+}
