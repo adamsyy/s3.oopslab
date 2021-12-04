1294c94ac923fa9978ba43f597a44ffc1302744e
adamsy <adamoommenjacob@gmail.com>
2021-12-04 17:34:33 +0530

    4

--- lab1/README.md
+++ lab1/README.md
@@ -3,3 +3,6 @@ Example.java : simple Java program to show printing
 Example2.java: kinda same as before
 </br>
 Example3.java: just to show how input works
+</br>
+Sumofdigits.java: find sum of digits of a number
+</br>
---
+++ lab1/Sumofdigits.java
@@ -0,0 +1,19 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Sumofdigits {
+   public static void main(String[] args) {
+      
+         int num, sum = 0, rem;
+            System.out.println("Enter a number");
+            Scanner sc=new Scanner(System.in);
+            num = sc.nextInt();
+            while (num > 0) {
+                rem = num % 10;
+                sum = sum + rem;
+                num = num / 10;
+            }
+            System.out.println("Sum of digits of the number is " + sum);
+   }
+}
