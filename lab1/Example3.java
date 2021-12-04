7317db1f258f12777b7894e9570e30d46fc5a6aa
adamsy <adamoommenjacob@gmail.com>
2021-12-04 17:30:54 +0530

    1.3

---
+++ lab1/Example3.java
@@ -0,0 +1,12 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the element");
+        int n=sc.nextInt();
+        System.out.println("element is"+n);
+    }
+}
--- lab1/README.md
+++ lab1/README.md
@@ -2,3 +2,4 @@ Example.java : simple Java program to show printing
 </br>
 Example2.java: kinda same as before
 </br>
+Example3.java: just to show how input works
